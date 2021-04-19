2d0b2c3704250632a77f4f179b9ccdaa2ea38a7b
WH Seo <ozaki23@naver.com>
2021-04-19 14:43:51 +0900

    branchB 생성 (branchB 커밋1)

--- GitStudy/src/exam/Qusetion2.java
+++ GitStudy/src/exam/Qusetion2.java
@@ -4,5 +4,8 @@ public class Qusetion2 {
 	public static void main(String[] args) {
 		System.out.println(Math.random());
 		System.out.println("Random");
+		
+		System.out.println("branchB »ý¼º ±â³ä");
+		
 	}
 }
