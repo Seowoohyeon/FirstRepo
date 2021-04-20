ad9984633afc270601a80b81bf53b4568fc57a3c
J W Jeong <jjw3410@naver.com>
2021-04-20 09:49:01 +0900

    good

--- GitStudy/src/exam/Answer1.java
+++ GitStudy/src/exam/Answer1.java
@@ -5,6 +5,9 @@ public class Answer1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("aaaaaa");
+		
+		
+		System.out.println("good");
 	}
 
 }
